6a6346231d76d2c4abc612c517bfe5e352ef53e0
nickporjonathan <nickporjonathan@gmail.com>
2022-04-18 13:27:43 -0400

    get product created

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,7 +19,16 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+    where: { id: req.params.id },
+    include: Category && Tag,
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}); 
 
 // create new product
 router.post("/", (req, res) => {
