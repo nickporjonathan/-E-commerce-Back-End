15a87eb212ad5879a59588ca968ae85a9fc09ef6
nickporjonathan <nickporjonathan@gmail.com>
2022-04-18 14:02:31 -0400

    implemented delete/destroy in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,7 +28,7 @@ router.get("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-}); 
+});
 
 // create new product
 router.post("/", (req, res) => {
@@ -106,6 +106,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "Product doesn't exist" });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
